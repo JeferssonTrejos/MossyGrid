a2684fe9f26674674858f6a7ddd88061ffca86c4
jeFordev <jeferssonfordev@gmail.com>
2024-05-28 20:15:11 -0600

    fix

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express();
 app.use(cors())
 app.use('/', express.static('public'))
 
+app.get('/ping', (req, res) => {
+    res.send({ ping: 'pong' })
+})
+
 // Connection
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
